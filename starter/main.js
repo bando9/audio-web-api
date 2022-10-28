c6dca6e2a9f72baeb2485eaaf0ae1d1623251e07
bando9 <bandomega1@gmail.com>
2022-10-28 15:26:09 +0700

    menambah kodingan di main.js, style.css, dan index.html

--- starter/main.js
+++ starter/main.js
@@ -1,1 +1,25 @@
 import './style.css'
+
+const AudioContext = window.AudioContext ?? window.webkitAudioContext;
+const audioCtx = new AudioContext()
+
+const audioElement = document.getElementById('audio');
+const playBtn = document.getElementById('playbtn');
+const volumeSlider = document.getElementById('volume');
+const seeker = document.getElementById('seeker');
+const time = document.getElementById('time');
+const duration = document.getElementById('duration')
+
+const audioSource = audioCtx.createMediaElementSource(audioElement)
+
+playBtn.addEventListener('click', (event) => {
+    const targetEl = event.target;
+
+    if (targetEl.getAttribute('class') == 'paused') {
+        audioElement.play();
+        targetEl.setAttribute('class', 'playing')
+    } else if (targetEl.getAttribute('class') == 'playing') {
+        audioElement.pause();
+        targetEl.setAttribute('class', 'paused')
+    }
+});
